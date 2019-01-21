7a9b675a79f8b3a9297b3729dd3fa55bcc661b99
Elcio Santos <elcio.santos@invillia.com>
2019-01-21 10:22:20 -0200

    teste

--- src/ESX.Teste.API/Controllers/PatrimoniosController.cs
+++ src/ESX.Teste.API/Controllers/PatrimoniosController.cs
@@ -19,7 +19,7 @@ namespace ESX.Teste.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return ResponseOk(_patrimonioAppService.GetAll());
+            return ResponseOk(Environment.MachineName);
         }
 
         [HttpGet]
